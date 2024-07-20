c0e3917dad6d3c2445128438d1dadd990f36c9a5
tjtjdnjswhd <tjtjdnjswhd@gmail.com>
2024-07-20 15:27:39 +0900

    Project init

---
+++ src/main/java/com/example/journeyednt/JourneyedntApplication.java
@@ -0,0 +1,13 @@
+package com.example.journeyednt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyedntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyedntApplication.class, args);
+	}
+
+}
