94b343086279129e0ad8f105f0b62b2b9d6383a1
tjtjdnjswhd <tjtjdnjswhd@gmail.com>
2024-08-03 17:27:11 +0900

    #51 Feature: PostImage Repository, Service, Collection 수정
    - Repository:
      - 사용하지 않는 메서드 제거
    - Service:
      - 사용하지 않는 메서드 제거
    - Controller:
      - Post 연관해 반환하는 엔드포인트 제거. ResponseEntity<Resource>로 파일 획득

--- src/main/java/com/example/journeyednt/controller/PostImageController.java
+++ src/main/java/com/example/journeyednt/controller/PostImageController.java
@@ -3,71 +3,25 @@ package com.example.journeyednt.controller;
 import com.example.journeyednt.entity.PostImage;
 import com.example.journeyednt.service.PostImageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-
-@Controller
+@RestController
 @RequestMapping("/api/post-images")
 @RequiredArgsConstructor
 public class PostImageController {
 
     private final PostImageService postImageService;
 
-    // READ: postId에 해당하는 PostImage 목록을 반환
-    @GetMapping("/post/{postId}")
-    public String getPostImagesByPostId(@PathVariable Integer postId, Model model) {
-        List<PostImage> postImages = postImageService.getPostImagesByPostId(postId);
-        model.addAttribute("postImages", postImages);
-        return "postImage/list";
-    }
-
-    // READ: 특정 contentType에 해당하는 PostImage 목록을 반환
-    @GetMapping("/content-type/{contentType}")
-    public String getPostImagesByContentType(@PathVariable String contentType, Model model) {
-        List<PostImage> postImages = postImageService.getPostImagesByContentType(contentType);
-        model.addAttribute("postImages", postImages);
-        return "postImage/listByContentType";
-    }
-
-    // READ: postId에 해당하는 메인 PostImage를 반환
-    @GetMapping("/post/{postId}/primary")
-    public String getPrimaryPostImageByPostId(@PathVariable Integer postId, Model model) {
-        Optional<PostImage> postImage = postImageService.getPrimaryPostImageByPostId(postId);
-        model.addAttribute("postImage", postImage.orElse(null)); // null  Optional<PostImage>가 비어 있을 경우에 대한 처리
-        return "postImage/primary";
-    }
-
-    // READ: postId와 contentType에 해당하는 PostImage 목록을 반환
-    @GetMapping("/post/{postId}/content-type/{contentType}")
-    public String getPostImagesByPostIdAndContentType(@PathVariable Integer postId, @PathVariable String contentType, Model model) {
-        List<PostImage> postImages = postImageService.getPostImagesByPostIdAndContentType(postId, contentType);
-        model.addAttribute("postImages", postImages);
-        return "postImage/listByPostIdAndContentType";
-    }
-
-    // GET: id에 해당하는 PostImage 편집 폼을 반환
-    @GetMapping("/{id}/edit")
-    public String editForm(@PathVariable("id") Integer id, Model model) {
+    @GetMapping("/{id}")
+    public ResponseEntity<Resource> getPostImagesByPostId(@PathVariable Integer id) {
         PostImage postImage = postImageService.findById(id);
-        model.addAttribute("postImage", postImage);
-        return "postImage/edit";
-    }
+        ByteArrayResource resource = new ByteArrayResource(postImage.getData());
+        MediaType mediaType = MediaType.parseMediaType(postImage.getContentType());
 
-    // POST: id에 해당하는 PostImage 편집 폼에서 데이터 제출
-    @PostMapping("/{id}/edit")
-    public String edit(@PathVariable("id") Integer id, @ModelAttribute PostImage updatedPostImage) {
-        postImageService.updatePostImage(id, updatedPostImage.getContentType(), updatedPostImage.getData(), updatedPostImage.getIsPrimary());
-        return "redirect:/post-images/post/" + id;
-    }
-
-    // DELETE: id에 해당하는 PostImage를 삭제
-    @GetMapping("/{id}/delete")
-    public String deletePostImage(@PathVariable("id") Integer id) {
-        postImageService.deletePostImageById(id);
-        return "redirect:/post-images";
+        return ResponseEntity.ok().contentType(mediaType).body(resource);
     }
 }
