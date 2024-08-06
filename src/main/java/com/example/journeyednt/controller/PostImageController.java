4ec5803b696a13abc7c3d4e429a95d6b76edd283
tjtjdnjswhd <tjtjdnjswhd@gmail.com>
2024-08-06 22:09:00 +0900

    Fix: 기본이미지

--- src/main/java/com/example/journeyednt/controller/PostImageController.java
+++ src/main/java/com/example/journeyednt/controller/PostImageController.java
@@ -4,6 +4,7 @@ import com.example.journeyednt.entity.PostImage;
 import com.example.journeyednt.service.PostImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -27,11 +28,18 @@ public class PostImageController {
 
     @GetMapping("/posts/{postId}")
     public ResponseEntity<Resource> getPostPrimaryImageByPostId(@PathVariable Integer postId) {
-        Integer imageId = postImageService.getPostPrimaryImageByPostId(postId);
-        PostImage postImage = postImageService.findById(imageId);
-        ByteArrayResource resource = new ByteArrayResource(postImage.getData());
-        MediaType mediaType = MediaType.parseMediaType(postImage.getContentType());
+        try {
+            Integer imageId = postImageService.getPostPrimaryImageByPostId(postId);
+            PostImage postImage = postImageService.findById(imageId);
+            ByteArrayResource resource = new ByteArrayResource(postImage.getData());
+            MediaType mediaType = MediaType.parseMediaType(postImage.getContentType());
 
-        return ResponseEntity.ok().contentType(mediaType).body(resource);
+            return ResponseEntity.ok().contentType(mediaType).body(resource);
+        } catch (Exception e) {
+            ClassPathResource classPathResource = new ClassPathResource("static/img/mainImg.png");
+            MediaType mediaType = MediaType.IMAGE_PNG;
+
+            return ResponseEntity.ok().contentType(mediaType).body(classPathResource);
+        }
     }
 }
